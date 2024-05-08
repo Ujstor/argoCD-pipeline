9a94047980b90e31c97e231bdf55b2781fcdb6dd
Ujstor <astipan@gmail.com>
2024-05-08 02:24:47 +0200

    err hand

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	html := `<html><head><title>Test1</title></head><body><h1>Welcome to Test1</h1></body></html>`
-	w.Write([]byte(html))
+	_, err := w.Write([]byte(html))
+	if err != nil {
+		http.Error(w, "Error writing response", http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
 	http.HandleFunc("/", handler)
 
-	http.ListenAndServe(":8088", nil)
+	err := http.ListenAndServe(":8088", nil)
+	if err != nil {
+		log.Fatalf("Error starting server: %s", err)
+	}
 }
